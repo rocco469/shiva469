2664f295184b30746c640eb6abf99d3c2cedd5d2
rocco469 <manuelsernaperez@gmail.com>
2024-01-21 01:13:05 -0500

    Tu mensaje descriptivo del commi rocc sueñot22

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>roccoaa!ss432424</Text>
+      <Text>rocco tiene sueño</Text>
       <StatusBar style="auto" />
     </View>
   );
