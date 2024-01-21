43e480076def5a3b5c03eba9923bfa20b5362520
rocco469 <manuelsernaperez@gmail.com>
2024-01-21 01:05:30 -0500

    Tu mensaje descriptivo del commi msst

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>piru!</Text>
+      <Text>pirurocc!</Text>
       <StatusBar style="auto" />
     </View>
   );
