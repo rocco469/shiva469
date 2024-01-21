b8a29922ea0e591e15eee77e2304bf53f485a0d8
unknown <manuelsernaperez@gmail.com>
2024-01-21 01:19:54 -0500

    rtrtrtd

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>rocco tiene sueño y no me da polagit status</Text>
+      <Text>rocco tiene sueño y no me da polagit status ewerwerewrewr</Text>
       <StatusBar style="auto" />
     </View>
   );
